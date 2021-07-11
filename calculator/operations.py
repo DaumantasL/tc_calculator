a7e8dfd144fab26f88b204cd850c359adb2440ca
DaumantasL <75472196+DaumantasL@users.noreply.github.com>
2021-07-11 21:36:10 +0100

    Update operations.py

--- calculator/operations.py
+++ calculator/operations.py
@@ -6,8 +6,8 @@ class CalculatorError(Exception):
 class calculator:
     """A memory-based basic calculator."""
     def __init__(self, memory: float = 0) -> None:
-        """Initialize calculator with a number to perform operations on"""
-        self._check_input(memory)
+        """Initialize calculator with a number to perform operations on. Zero by default."""
+        self._check_argument(memory)
         self.__memory = memory
 
     @property
@@ -18,7 +18,7 @@ class calculator:
     @memory.setter
     def memory(self, new_memory: float):
         """Sets calculator memory to number value provided"""
-        self._check_input(new_memory)
+        self._check_argument(new_memory)
         self.__memory = new_memory
 
     @memory.deleter
@@ -34,25 +34,25 @@ class calculator:
 
     def add(self, argument:float):
         """Adds number to number in calculator memory"""
-        self._check_input(argument)
+        self._check_argument(argument)
         self.__memory = self.__memory + argument
         return self.__memory
     
     def subtract(self, argument:float):
         """Subtracts number from number in calculator memory"""
-        self._check_input(argument)
+        self._check_argument(argument)
         self.__memory = self.__memory - argument
         return self.__memory
 
     def multiply(self, argument:float):
         """Multiplies number in calculator memory by number"""
-        self._check_input(argument)
+        self._check_argument(argument)
         self.__memory = self.__memory * argument
         return self.__memory
 
     def divide(self, argument:float):
         """Divides number in calculator memory by number"""
-        self._check_input(argument)
+        self._check_argument(argument)
         try:
             self.__memory = self.__memory / argument
             return self.__memory
@@ -61,7 +61,7 @@ class calculator:
 
     def root(self, argument:float):
         """Takes n-th root of number in calculator memory"""
-        self._check_input(argument)
+        self._check_argument(argument)
         try:
             self.__memory = self.__memory ** 1/argument
             return self.__memory
@@ -69,12 +69,12 @@ class calculator:
             raise CalculatorError("You can not take a 0th root.") from ex
     
     def power(self, argument:float):
-        """Takes n-th root of number in calculator memory"""
-        self._check_input(argument)
+        """Raises number in calculator memory to n-th power"""
+        self._check_argument(argument)
         self.__memory = self.__memory ** argument
         return self.__memory
 
-    def _check_input(self, input):
-        """Check that the operand is a number."""
-        if not isinstance(input, numbers.Number):
-            raise CalculatorError(f'"{input}" is not a number.')
+    def _check_argument(self, argument):
+        """Checks whether argument is a number."""
+        if not isinstance(argument, numbers.Number):
+            raise CalculatorError(f'"{argument}" is not a number.')
