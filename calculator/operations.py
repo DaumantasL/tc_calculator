d4e0612dd3a89d3dd79f478fba3499dad92b8492
DaumantasL <75472196+DaumantasL@users.noreply.github.com>
2021-07-11 13:05:47 +0100

    Update operations.py

--- calculator/operations.py
+++ calculator/operations.py
@@ -1,7 +1,11 @@
+class CalculatorError(Exception):
+    """For calculator errors"""
+
 class calculator:
     """A memory-based basic calculator."""
     def __init__(self, memory: float = 0) -> None:
         """Initialize calculator with a number to perform operations on"""
+        self._check_input(memory)
         self.__memory = memory
 
     @property
@@ -12,6 +16,7 @@ class calculator:
     @memory.setter
     def memory(self, new_memory: float):
         """Sets calculator memory to number value provided"""
+        self._check_input(new_memory)
         self.__memory = new_memory
 
     @memory.deleter
@@ -27,34 +32,49 @@ class calculator:
 
     def add(self, argument:float):
         """Adds number to number in calculator memory"""
+        self._check_input(argument)
         self.__memory = self.__memory + argument
         return self.__memory
     
     def subtract(self, argument:float):
         """Subtracts number from number in calculator memory"""
+        self._check_input(argument)
         self.__memory = self.__memory - argument
         return self.__memory
 
     def multiply(self, argument:float):
         """Multiplies number in calculator memory by number"""
+        self._check_input(argument)
         self.__memory = self.__memory * argument
         return self.__memory
 
     def divide(self, argument:float):
         """Divides number in calculator memory by number"""
-        self.__memory = self.__memory / argument
-        return self.__memory
+        self._check_input(argument)
+        try:
+            self.__memory = self.__memory / argument
+            return self._memory
+        except ZeroDivisionError as ex:
+            raise CalculatorError("You can't divide by zero.") from ex
 
     def root(self, argument:float):
         """Takes n-th root of number in calculator memory"""
-        self.__memory = self.__memory ** 1/argument
-        return self.__memory
+        self._check_input(argument)
+        try:
+            self.__memory = self.__memory ** 1/argument
+            return self._memory
+        except ZeroDivisionError as ex:
+            raise CalculatorError("You can't divide by zero.") from ex
     
     def power(self, argument:float):
         """Takes n-th root of number in calculator memory"""
+        self._check_input(argument)
         self.__memory = self.__memory ** argument
         return self.__memory
 
-
-
-    
+    def _check_input(self, input):
+        """Check that input is a number."""
+        try:
+            float(input)
+        except ValueError:
+            return CalculatorError(f'"{input}" is not a number.')
